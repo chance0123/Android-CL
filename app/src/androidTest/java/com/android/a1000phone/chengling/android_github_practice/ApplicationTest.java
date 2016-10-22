b59bdec2c2329893a18f2ddcf9ae5636b8977827
chance0123 <cl_eof@163.com>
2016-10-22 15:12:38 +0800

    This is null project

---
+++ app/src/androidTest/java/com/android/a1000phone/chengling/android_github_practice/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.android.a1000phone.chengling.android_github_practice;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
