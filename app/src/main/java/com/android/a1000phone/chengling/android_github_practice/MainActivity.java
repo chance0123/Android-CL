6056371041d54a6f14cc1cf8d1648854a3fbda38
chance0123 <cl_eof@163.com>
2016-10-22 15:48:33 +0800

    cl第三次修改

--- app/src/main/java/com/android/a1000phone/chengling/android_github_practice/MainActivity.java
+++ app/src/main/java/com/android/a1000phone/chengling/android_github_practice/MainActivity.java
@@ -21,4 +21,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
